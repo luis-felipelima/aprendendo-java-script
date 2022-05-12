877f0b0b221f2ed959141b9c0a3f059b3dc23f42
Luis Felipe <luis.felipe.lima23@gmail.com>
2022-05-12 18:24:43 -0300

    Aprendizado JavaScript

--- listas2.js
+++ listas2.js
@@ -1,17 +1,28 @@
 console.log(`Trabalhando com listas`)
 
-const idadeComprador = 18;
+const temPassagemComprada = true;
+const idadeComprador = 17;
+const estaAcompanhada = true;
 const listaDestinos = new Array(
     `São Paulo`,
     `Salvador`,
     `Rio de Janeiro` 
 );
 
-if(idadeComprador >= 18){
+if(idadeComprador >= 18 || estaAcompanhada == true){
     listaDestinos.splice(0,1);
-    console.log("Sua passagem foi comprada")
-}else {
-    console.log("Não podemos vender para pessoas menores de idade")
+    console.log("Sua passagem foi comprada");
 }
+else {
+    console.log("Não podemos vender para pessoas menores de idade");
+    
+}
+
+if(temPassagemComprada == true){
+    console.log("Pode embarcar, Boa viagem!")
+}else{
+    console.log("Você não pode embarcar sem estar com a passagem comprada")
+}
+
 
 console.log(listaDestinos);
